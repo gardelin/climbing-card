ac247c602bf9ee4f7055a6209f16047fdcae71e7
rlabrovi <roko.labrovic@gmail.com>
2022-01-30 17:10:36 +0100

    Start observer only if needed

--- resources/assets/js/frontend.js
+++ resources/assets/js/frontend.js
@@ -1,8 +1,6 @@
-// require('./climbing-card')
 import ClimbingCard from './ClimbingCard';
 import LastClimbedRoutes from './LastClimbedRoutes';
 import Counter from './Counter';
-import Utils from './Utils';
 
 document.addEventListener('DOMContentLoaded', function () {
     window.ClimbingCard = new ClimbingCard();
@@ -10,28 +8,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let counters = document.querySelectorAll('.counter');
 
-    var observer = new IntersectionObserver(
-        function (entries) {
-            if (entries[0].isIntersecting === true) {
-                this.disconnect();
+    if (counters.length) {
+        var observer = new IntersectionObserver(
+            function (entries) {
+                if (entries[0].isIntersecting === true) {
+                    this.disconnect();
 
-                counters.forEach(counter => {
-                    let type = counter.getAttribute('data-type');
-                    let start = counter.getAttribute('data-start');
-                    let end = counter.getAttribute('data-end');
-                    let duration = counter.getAttribute('data-duration');
+                    counters.forEach(counter => {
+                        let type = counter.getAttribute('data-type');
+                        let start = counter.getAttribute('data-start');
+                        let end = counter.getAttribute('data-end');
+                        let duration = counter.getAttribute('data-duration');
 
-                    new Counter(counter, {
-                        type: type,
-                        start: start,
-                        end: end,
-                        duration: parseInt(duration),
+                        new Counter(counter, {
+                            type: type,
+                            start: start,
+                            end: end,
+                            duration: parseInt(duration),
+                        });
                     });
-                });
-            }
-        },
-        {threshold: [1]},
-    );
+                }
+            },
+            {threshold: [0.5], root: document},
+        );
 
-    observer.observe(document.querySelector('.club-stats-counters'));
+        observer.observe(document.querySelector('.club-stats-counters'));
+    }
 });
