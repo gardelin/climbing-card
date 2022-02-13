755d91f9c9a73cda65f504008f45cd42feb40433
rlabrovi <roko.labrovic@gmail.com>
2022-02-13 09:34:08 +0100

    Removing unused font

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ mix.options({
 // Define the public and resources paths
 mix.setPublicPath('public/assets');
 mix.setResourceRoot('../');
-mix.options({ processCssUrls: false });
 
 // Compile admin scripts and styles
 mix.js(['resources/assets/js/frontend/frontend.js'], 'public/assets/js');
@@ -22,7 +21,6 @@ mix.js('resources/assets/js/admin/admin.js', 'public/assets/js').vue();
 mix.sass('resources/assets/sass/app.scss', 'public/assets/css');
 mix.sass('resources/assets/sass/frontend.scss', 'public/assets/css');
 mix.sass('resources/assets/sass/admin.scss', 'public/assets/css');
-mix.copy('resources/assets/fonts', 'public/assets/fonts');
 
 mix.sourceMaps();
 mix.browserSync('https://vertikal.local');
