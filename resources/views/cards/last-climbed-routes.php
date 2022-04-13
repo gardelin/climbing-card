d53b7175b61f3672018d3456c0c615251cfbbdb1
rlabrovi <roko.labrovic@gmail.com>
2022-04-13 12:06:26 +0200

    last_climbed_routes shortcode update

--- resources/views/cards/last-climbed-routes.php
+++ resources/views/cards/last-climbed-routes.php
@@ -10,9 +10,17 @@
                         <span>
                             <?php echo $card->user_fullname; ?>
                         </span>
-                        <div class="climbed-data">
-                            <div><?php echo $card->ocjena; ?></div>
-                            <span class="dot <?php echo str_replace(" ", "-", $card->style); ?> title=" <?php echo $card->style; ?>"></span>
+                        <div class="info">
+                            <?php if (isset($options['show-route-in-header']) && $options['show-route-in-header'] == 1) : ?>
+                                <span><?php echo $card->route; ?></span>
+                            <?php endif; ?>
+                            <?php if (isset($options['show-date-in-header']) && $options['show-date-in-header'] == 1) : ?>
+                                <span><?php echo $card->climbed_at; ?></span>
+                            <?php endif; ?>
+                            <div class="climbed-data">
+                                <div><?php echo $card->ocjena; ?></div>
+                                <span class="dot <?php echo str_replace(" ", "-", $card->style); ?> title=" <?php echo $card->style; ?>"></span>
+                            </div>
                         </div>
                     </div>
                     <div class="climbed-content ac-panel">
@@ -20,7 +28,7 @@
                         <span><?php printf(__('<strong>Ocjena:</strong> %s', 'climbingcard'), $card->grade); ?></span>
                         <span><?php printf(__('<strong>Penjalište:</strong> %s', 'climbingcard'), $card->crag); ?></span>
                         <span><?php printf(__('<strong>Način:</strong> %s', 'climbingcard'), $card->style); ?></span>
-                        <span><?php printf(__('<strong>Datum:</strong> %s', 'climbingcard'), $card->date); ?></span>
+                        <span><?php printf(__('<strong>Datum:</strong> %s', 'climbingcard'), $card->climbed_at); ?></span>
                     </div>
                 </div>
             <?php endforeach; ?>
--- src/Shortcodes/LastClimbedRoutes.php
+++ src/Shortcodes/LastClimbedRoutes.php
@@ -19,14 +19,22 @@ class LastClimbedRoutes
     public static function render($attributes = [], $content = null): string
     {
         $number = isset($attributes['number']) ? (int) $attributes['number'] : 10;
-
         $cards = Cards::getInstance()->all(['limit' => $number]);
+        $data = [];
 
         foreach ($cards as $card) {
+            $isClimbingCardPublic = get_user_meta($card->user_id, 'is_climbing_card_public', true) ?: 'true';
+
+            if (!(bool) json_decode($isClimbingCardPublic)) {
+                continue;
+            }
+
             $user = get_userdata($card->user_id);
             $card->user_fullname = trim($user->first_name . " " . $user->last_name);
+
+            array_push($data, $card);
         }
 
-        return view('cards/last-climbed-routes', ['cards' => $cards], false);
+        return view('cards/last-climbed-routes', ['cards' => $data, 'options' => $attributes], false);
     }
 }
