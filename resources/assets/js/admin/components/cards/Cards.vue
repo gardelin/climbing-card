5a004a014b470fd2c755ba5f8fcd6d3e327c34ab
rlabrovi <roko.labrovic@gmail.com>
2022-04-13 11:08:36 +0200

    Covering scenario when user don't have any route in climbing cardboard

--- resources/assets/js/admin/components/cards/Cards.vue
+++ resources/assets/js/admin/components/cards/Cards.vue
@@ -1,101 +1,104 @@
 <template>
-    <div class="table-header">
-        <div class="search-container">
-            <Search :size="18" />
-            <input class="search" type="text" :placeholder="$gettext('Search Cards')" v-model="searchQuery" />
-        </div>
-        <div class="date-container">
-            <div class="input-group">
-                <flat-pickr v-model="dateRange" :config="config" :placeholder="$gettext('Select Dates')"> </flat-pickr>
-                <X :size="18" v-if="dateRange" @click="dateRange = null" />
+    <div v-if="cards.length">
+        <div class="table-header">
+            <div class="search-container">
+                <Search :size="18" />
+                <input class="search" type="text" :placeholder="$gettext('Search Cards')" v-model="searchQuery" />
+            </div>
+            <div class="date-container">
+                <div class="input-group">
+                    <flat-pickr v-model="dateRange" :config="config" :placeholder="$gettext('Select Dates')"> </flat-pickr>
+                    <X :size="18" v-if="dateRange" @click="dateRange = null" />
+                </div>
             </div>
         </div>
+        <table class="cards">
+            <thead>
+                <tr>
+                    <th class="export">
+                        <input type="checkbox" v-model="selectAll" @change="_selectAll()" />
+                    </th>
+                    <th class="route" @click="_sort('route')">
+                        {{ $gettext('Route') }}
+                        <ChevronUp :size="15" v-if="this.sortBy === 'route' && !this.asc" />
+                        <ChevronDown :size="15" v-if="this.sortBy === 'route' && this.asc" />
+                    </th>
+                    <th class="crag" @click="_sort('crag')">
+                        {{ $gettext('Crag') }}
+                        <ChevronUp :size="15" v-if="this.sortBy === 'crag' && !this.asc" />
+                        <ChevronDown :size="15" v-if="this.sortBy === 'crag' && this.asc" />
+                    </th>
+                    <th class="grade" @click="_sort('grade')">
+                        {{ $gettext('Grade') }}
+                        <ChevronUp :size="15" v-if="this.sortBy === 'grade' && !this.asc" />
+                        <ChevronDown :size="15" v-if="this.sortBy === 'grade' && this.asc" />
+                    </th>
+                    <th class="style" @click="_sort('style')">
+                        {{ $gettext('Style') }}
+                        <ChevronUp :size="15" v-if="this.sortBy === 'style' && !this.asc" />
+                        <ChevronDown :size="15" v-if="this.sortBy === 'style' && this.asc" />
+                    </th>
+                    <th class="comment">{{ $gettext('Comment') }}</th>
+                    <th class="climbed_at" @click="_sort('climbed_at')">
+                        {{ $gettext('Date') }}
+                        <ChevronUp :size="15" v-if="this.sortBy === 'climbed_at' && !this.asc" />
+                        <ChevronDown :size="15" v-if="this.sortBy === 'climbed_at' && this.asc" />
+                    </th>
+                    <th class="actions"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="item" :class="{ edit: card.editmode === true }" v-for="card in filteredCards" :key="card.id">
+                    <td class="export" data-name="{{ $gettext('Export') }}">
+                        <input type="checkbox" v-if="!card.editmode" v-model="selected" :value="card" number />
+                    </td>
+                    <td class="route" data-name="{{ $gettext('Route') }}">
+                        <input v-if="card.editmode" v-model="card.route" :placeholder="$gettext('Route Name')" />
+                        <span v-else>{{ card.route }}</span>
+                        <span v-if="card.errors.route" class="error">{{ card.errors.route }}</span>
+                    </td>
+                    <td class="crag" data-name="{{ $gettext('Crag') }}">
+                        <input v-if="card.editmode" v-model="card.crag" :placeholder="$gettext('Crag - Sector')" />
+                        <span v-else>{{ card.crag }}</span>
+                        <span v-if="card.errors.crag" class="error">{{ card.errors.crag }}</span>
+                    </td>
+                    <td class="grade" data-name="{{ $gettext('Grade') }}">
+                        <select v-if="card.editmode" v-model="card.grade">
+                            <option value="" disabled selected>{{ $gettext('Select Grade') }}</option>
+                            <option v-for="grade in grades" :key="grade">
+                                {{ grade }}
+                            </option>
+                        </select>
+                        <span v-else>{{ card.grade }}</span>
+                        <span v-if="card.errors.grade" class="error">{{ card.errors.grade }}</span>
+                    </td>
+                    <td class="style" data-name="{{ $gettext('Style') }}">
+                        <select v-if="card.editmode" v-model="card.style">
+                            <option value="red point">Red Point</option>
+                            <option value="flash">Flash</option>
+                            <option value="on sight">On Sight</option>
+                        </select>
+                        <span v-else :class="card.style.replace(' ', '')">{{ card.style }}</span>
+                    </td>
+                    <td class="comment" data-name="{{ $gettext('Comment') }}">
+                        <textarea v-if="card.editmode" v-model="card.comment" />
+                        <span v-else>{{ card.comment }}</span>
+                    </td>
+                    <td class="climbed_at" data-name="{{ $gettext('Date') }}">
+                        <input v-if="card.editmode" type="date" v-model="card.climbed_at" />
+                        <span v-else>{{ card.climbed_at }}</span>
+                        <span v-if="card.errors.climbed_at" class="error">{{ card.errors.climbed_at }}</span>
+                    </td>
+                    <td class="actions" data-name="{{ $gettext('Actions') }}">
+                        <Edit2 :size="18" v-if="!card.editmode" @click="_edit(card)" />
+                        <Save :size="18" v-else @click="_save(card)" />
+                        <Trash2 :size="18" @click="_delete(card)" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-    <table class="cards">
-        <thead>
-            <tr>
-                <th class="export">
-                    <input type="checkbox" v-model="selectAll" @change="_selectAll()" />
-                </th>
-                <th class="route" @click="_sort('route')">
-                    {{ $gettext('Route') }}
-                    <ChevronUp :size="15" v-if="this.sortBy === 'route' && !this.asc" />
-                    <ChevronDown :size="15" v-if="this.sortBy === 'route' && this.asc" />
-                </th>
-                <th class="crag" @click="_sort('crag')">
-                    {{ $gettext('Crag') }}
-                    <ChevronUp :size="15" v-if="this.sortBy === 'crag' && !this.asc" />
-                    <ChevronDown :size="15" v-if="this.sortBy === 'crag' && this.asc" />
-                </th>
-                <th class="grade" @click="_sort('grade')">
-                    {{ $gettext('Grade') }}
-                    <ChevronUp :size="15" v-if="this.sortBy === 'grade' && !this.asc" />
-                    <ChevronDown :size="15" v-if="this.sortBy === 'grade' && this.asc" />
-                </th>
-                <th class="style" @click="_sort('style')">
-                    {{ $gettext('Style') }}
-                    <ChevronUp :size="15" v-if="this.sortBy === 'style' && !this.asc" />
-                    <ChevronDown :size="15" v-if="this.sortBy === 'style' && this.asc" />
-                </th>
-                <th class="comment">{{ $gettext('Comment') }}</th>
-                <th class="climbed_at" @click="_sort('climbed_at')">
-                    {{ $gettext('Date') }}
-                    <ChevronUp :size="15" v-if="this.sortBy === 'climbed_at' && !this.asc" />
-                    <ChevronDown :size="15" v-if="this.sortBy === 'climbed_at' && this.asc" />
-                </th>
-                <th class="actions"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="item" :class="{ edit: card.editmode === true }" v-for="card in filteredCards" :key="card.id">
-                <td class="export" data-name="{{ $gettext('Export') }}">
-                    <input type="checkbox" v-if="!card.editmode" v-model="selected" :value="card" number />
-                </td>
-                <td class="route" data-name="{{ $gettext('Route') }}">
-                    <input v-if="card.editmode" v-model="card.route" :placeholder="$gettext('Route Name')" />
-                    <span v-else>{{ card.route }}</span>
-                    <span v-if="card.errors.route" class="error">{{ card.errors.route }}</span>
-                </td>
-                <td class="crag" data-name="{{ $gettext('Crag') }}">
-                    <input v-if="card.editmode" v-model="card.crag" :placeholder="$gettext('Crag - Sector')" />
-                    <span v-else>{{ card.crag }}</span>
-                    <span v-if="card.errors.crag" class="error">{{ card.errors.crag }}</span>
-                </td>
-                <td class="grade" data-name="{{ $gettext('Grade') }}">
-                    <select v-if="card.editmode" v-model="card.grade">
-                        <option value="" disabled selected>{{ $gettext('Select Grade') }}</option>
-                        <option v-for="grade in grades" :key="grade">
-                            {{ grade }}
-                        </option>
-                    </select>
-                    <span v-else>{{ card.grade }}</span>
-                    <span v-if="card.errors.grade" class="error">{{ card.errors.grade }}</span>
-                </td>
-                <td class="style" data-name="{{ $gettext('Style') }}">
-                    <select v-if="card.editmode" v-model="card.style">
-                        <option value="red point">Red Point</option>
-                        <option value="flash">Flash</option>
-                        <option value="on sight">On Sight</option>
-                    </select>
-                    <span v-else :class="card.style.replace(' ', '')">{{ card.style }}</span>
-                </td>
-                <td class="comment" data-name="{{ $gettext('Comment') }}">
-                    <textarea v-if="card.editmode" v-model="card.comment" />
-                    <span v-else>{{ card.comment }}</span>
-                </td>
-                <td class="climbed_at" data-name="{{ $gettext('Date') }}">
-                    <input v-if="card.editmode" type="date" v-model="card.climbed_at" />
-                    <span v-else>{{ card.climbed_at }}</span>
-                    <span v-if="card.errors.climbed_at" class="error">{{ card.errors.climbed_at }}</span>
-                </td>
-                <td class="actions" data-name="{{ $gettext('Actions') }}">
-                    <Edit2 :size="18" v-if="!card.editmode" @click="_edit(card)" />
-                    <Save :size="18" v-else @click="_save(card)" />
-                    <Trash2 :size="18" @click="_delete(card)" />
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <p v-else>{{ $gettext('No routes found in your cartboard. You can add it using "Add" button above.') }}</p>
 </template>
 
 <script>
