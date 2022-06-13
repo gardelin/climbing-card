4dec4264ac03ae2104bb7341edc20e3f9ebd567b
rlabrovi <roko.labrovic@gmail.com>
2022-06-13 17:12:04 +0200

    Removing shortcode title (user can define it)

--- resources/views/cards/top-users-by-number-of-climbed-routes.php
+++ resources/views/cards/top-users-by-number-of-climbed-routes.php
@@ -2,7 +2,6 @@
 
 <?php if (isset($users) && !empty($users)) : ?>
     <div class="sidebar-widget top-users-by-number-of-climbed-routes">
-        <h4><?php _e('Top 10 e-kartona po broju popetih smjerova', 'climbingcard'); ?></h4>
         <div class="widget-container">
             <?php foreach ($users as $user) : ?>
                 <div><?php echo $user['fullname'] . ": "; ?><strong><?php echo $user['total']; ?></strong></div>
