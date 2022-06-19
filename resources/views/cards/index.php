31c4138c8375e6113d559c770b210f77ce15ab6e
rlabrovi <roko.labrovic@gmail.com>
2022-06-19 21:36:05 +0200

    Removing unused files

--- resources/views/cards/index.php
+++ resources/views/cards/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use function ClimbingCard\view;
-
 $repo = new ClimbingCard\Repositories\Cards;
 
 get_header();
@@ -10,15 +8,7 @@ get_header();
 
 <main class="wrapper section-inner group" id="site-content">
     <div class="posts">
-        <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-                get_template_part('content', get_post_type());
-                view('cards/partials/form');
-                view('cards/partials/table');
-            endwhile;
-        endif;
-        ?>
+        <div id="app"></div>
     </div><!-- .posts -->
 </main><!-- .wrapper -->
 
