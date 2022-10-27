afc465810ef252915b8723c21854e41b3fac0135
rlabrovi <roko.labrovic@gmail.com>
2022-10-27 14:24:30 +0200

    Don't allow user to add multiple new cards, only one can be added

--- resources/assets/js/admin/pages/CardsPage.vue
+++ resources/assets/js/admin/pages/CardsPage.vue
@@ -5,11 +5,11 @@
                 <div class="col">
                     <Header :title="$gettext('Climbing Cards')" :description="$gettext('Manage your climbed routes.')" />
                     <div style="display: flex">
-                        <div class="btn" v-if="store.getters['user/cards'].length" @click.prevent="store.dispatch('user/exportToCsv')">
+                        <div class="btn" v-if="store.getters['user/cards'].length" @click="store.dispatch('user/exportToCsv')">
                             <Download :size="16" />
                             {{ $gettext('Export CSV') }}
                         </div>
-                        <div class="btn" @click.prevent="store.dispatch('user/appendCard', newCardTemplate())">
+                        <div class="btn" @click="addNewCard">
                             <Plus :size="16" />
                             {{ $gettext('Add') }}
                         </div>
@@ -38,12 +38,14 @@
 
 <script setup>
     import { useStore } from 'vuex';
+    import { computed } from 'vue';
     import Cards from '../components/Cards';
     import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
     import Header from '../components/partials/Header';
     import { Plus, Download } from 'lucide-vue-next';
 
     const store = useStore();
+    const cards = computed(() => store.getters['user/cards']);
     const newCardTemplate = () => {
         return {
             id: '',
@@ -62,5 +64,22 @@
             },
             appendingNew: true,
         };
+    };
+
+    /**
+     * Add new card to table so user can fill it.
+     * Make sure that there is only one new card otherwise
+     * it can be confusing for user.
+     *
+     * @return {Void}
+     */
+    const addNewCard = () => {
+        let found = cards.value.find(item => {
+            return item.id === '';
+        });
+
+        if (!found) {
+            store.dispatch('user/appendCard', newCardTemplate());
+        }
     };
 </script>
